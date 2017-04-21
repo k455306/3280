a03ef26552f0a0840aa1b2936306c19d1a588014
JamesPainter <JamesPainter@mail.weber.edu>
2017-04-21 00:48:49 -0600

    Date Date

--- 3280_Group_Project/InvoiceRepository.cs
+++ 3280_Group_Project/InvoiceRepository.cs
@@ -233,7 +233,7 @@ namespace _3280_Group_Project
             try
             {
                 ///query string to bring back all invoices from database
-                string query = "SELECT * FROM [Invoices] WHERE [invoiceDate] = " + Date.ToString();
+                string query = "SELECT * FROM [Invoices] WHERE [invoiceDate] = " + Date.Date.ToString();
                 ///New Instance of OleDBCommand Command to database using query and connection 
                 OleDbCommand accessCommand = new OleDbCommand(query, OleDB);
                 ///New Instance of OleDBAdator to store results from accessDBCommand 
