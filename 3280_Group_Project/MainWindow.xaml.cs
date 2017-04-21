9c2436ce03970985a79983f6602995781badeaa7
k455306 <kyle.abel@gmail.com>
2017-04-20 23:37:18 -0600

    Main Ui additions

--- 3280_Group_Project/MainWindow.xaml.cs
+++ 3280_Group_Project/MainWindow.xaml.cs
@@ -22,62 +22,96 @@ namespace _3280_Group_Project
     {
         SearchWindow MySearchWindow;
         UpdateWindow MyUpdateWindow;
+
         List<Inventory> Inv;
         List<Customer> Customers;
+        List<Invoice> Invoices;
         InvoiceRepository iR;
+        decimal total;
 
         public MainWindow()
         {
+            //Initialize the window component
             InitializeComponent();
-            //populate customer list
-            //populate item list
+            //Applications stop running only when the Shutdown method of the Application is called
             Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+            // Initialize new Invoice Repository object
             iR = new InvoiceRepository();
+            // Initialize new Inventory list
             Inv = new List<Inventory>();
-            Inv = iR.getAllInventoryItems(); //Look at this line here
+            //Set Inventory items to list
+            Inv = iR.getAllInventoryItems();
+            //Initialize new customer list
             Customers = new List<Customer>();
+            //Set Customers to list
             Customers = iR.GetCustomers();
+            Invoices = new List<Invoice>();
+
             MySearchWindow = new SearchWindow();
             MyUpdateWindow = new UpdateWindow();
 
+            //Adds each item to the combo box
             foreach (Customer Customers in Customers)
             {
                 customerBox.Items.Add(Customers.ToString());
             }
 
-           foreach (Inventory Inv in Inv)
-           {
+             foreach (Inventory Inv in Inv)
+            {
                 itemBox.Items.Add(Inv.ToString());
-           }
+            }
             
 
         }
-
+        /// <summary>
+        /// Button click to bring up the search window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void searchBtn_Click(object sender, RoutedEventArgs e)
         {
-            //Brings up search window.
+            
             MySearchWindow.ShowDialog();
         }
-
+        /// <summary>
+        /// Button click to commit invoice to the db
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void submitBtn_Click(object sender, RoutedEventArgs e)
         {
             //Commits invoice to the DB. 
+            
+            //iR.AddInvoice();
         }
-
+        /// <summary>
+        /// Clears invoice screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void cancelBtn_Click(object sender, RoutedEventArgs e)
         {
-            //Cancels current invoice and clears all fields.
+            itemListBox.Items.Clear();
+            customerBox.SelectedIndex = -1;
+            itemBox.SelectedIndex = -1;
+            totalBox.Clear();
         }
-
+        /// <summary>
+        /// Brings up search window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            //Brings up search window.
             MySearchWindow.ShowDialog();
         }
-
+        /// <summary>
+        /// Closes program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
-            //Closes program
             this.Close();
         }
 
@@ -85,15 +119,45 @@ namespace _3280_Group_Project
         {
             MyUpdateWindow.ShowDialog();
         }
-
+        /// <summary>
+        /// Adds item to list box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void addItemBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            invTotal(itemBox.SelectedIndex,'a');
+            itemListBox.Items.Add(itemBox.Text);
+            totalBox.Text = total.ToString();
         }
-
+        /// <summary>
+        /// Deletes item from list box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void delItemBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            invTotal(itemBox.SelectedIndex,'s');
+            itemListBox.Items.Remove(itemListBox.SelectedItem);
+            totalBox.Text = total.ToString();
         }
+        /// <summary>
+        /// Calculates invoice total
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private void invTotal(int x, char y)
+        {
+            if(y == 'a')
+            {
+                total += Inv[x].ItemCost;
+            }
+            else if (y == 's')
+            {
+                total -= Inv[x].ItemCost;
+            }
+            
+        }
+
     }
 }
